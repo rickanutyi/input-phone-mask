8c9b183913e258e6020c9659eb2e836aa8ecede7
CH/Erlan <EChynybaev>
2024-01-25 14:49:40 +0600

    webpack, babel - build

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules")],
+    alias: {
+        'core': path.resolve(__dirname, 'src/core'),
+        'utils': path.resolve(__dirname, 'src/utils'),
+    }
+  },
+};
