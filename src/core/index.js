a2ff77b5a6b457bb99cd42f00b3a36e1f3822212
CH/Erlan <EChynybaev>
2024-01-25 12:13:46 +0600

    fix types

--- src/core/index.js
+++ src/core/index.js
@@ -6,7 +6,8 @@ import { isMutable, isNumber } from "utils";
  * @param {string} newValue - Значение из поля ввода
  * @returns {string}
  * @example
- * maskNewValue('+996-000-000-000', '12') => '+996-120-000-000'
+ * MutableReplaceChar = "_"
+ * maskNewValue('+996-000-000-000', '12') => '+996-12_-___-___'
  */
 export const maskNewValue = (mask, newValue) => {
   const unmaskedValue = newValue.replace(/\D/g, "").replace(/^(996)/, "");
